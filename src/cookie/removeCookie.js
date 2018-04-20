a34e4b80334bf5c45f14c0c7757e35c06e64d6e5
ztjy-fe <zhangzhenglong@szy.cn>
2018-04-20 14:55:54 +0800

    更新cookie

--- src/cookie/removeCookie.js
+++ src/cookie/removeCookie.js
@@ -3,10 +3,12 @@ var setCookie = require('./setCookie');
  * 
  * @desc 根据name删除cookie
  * @param  {String} name 
+ * @param {Object} params
  */
-function removeCookie(name) {
+function removeCookie(name, params) {
     // 设置已过期，系统会立刻删除cookie
-    setCookie(name, '', -1);
+    params.expires = -1;
+    setCookie(name, '', params);
 }
 
 module.exports = removeCookie
