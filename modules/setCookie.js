611a45d333efebfc212ed36f147bfdd454e62426
ztjy-fe <zhangzhenglong@szy.cn>
2018-04-23 09:59:14 +0800

    update build files

--- modules/setCookie.js
+++ modules/setCookie.js
@@ -3,12 +3,28 @@
  * @desc  设置Cookie
  * @param {String} name 
  * @param {String} value 
- * @param {Number} days 
+ * @param {Object} params 
  */
-function setCookie(name, value, days) {
-    var date = new Date();
-    date.setDate(date.getDate() + days);
-    document.cookie = name + '=' + value + ';expires=' + date + '; path=/';
+function setCookie(name, value, params) {
+    var stringifiedAttributes = '';
+
+    // 过期时间
+    if (params.expires === 'number') {
+    	var date = new Date();
+    	date.setDate(date.getDate() + params.expires);
+    	stringifiedAttributes +=';expires=' + date;
+    }
+
+    // path
+    var path = params.path ? params.path : '/';
+    stringifiedAttributes +=';path=' + path;
+
+    // domain
+    if (params.domain) {
+    	stringifiedAttributes +=';domain=' + params.domain;
+    }
+
+    document.cookie = name + '=' + value + stringifiedAttributes;
 }
 
 module.exports = setCookie
