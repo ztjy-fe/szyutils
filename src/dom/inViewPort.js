a578c75f052fb966d8926d103e53069c262af00e
zhangzhenglong <zhangzhenglong@szy.cn>
2018-12-24 17:50:18 +0800

    update

--- src/dom/inViewPort.js
+++ src/dom/inViewPort.js
@@ -7,7 +7,7 @@
 function isInViewPort(ele) {
     var rect = ele.getBoundingClientRect();
     var clientHeight = window.innerHeight || document.documentElement.clientHeight;
-    if (rect.top >= 0 && rect.bottom <= clientHeight) {
+    if (parseInt(rect.top) >= 0 && rect.bottom <= clientHeight) {
         return true;
     }
     return false;
